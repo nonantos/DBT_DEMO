5e705cc3b3d3b893af2417ce95b84e9461f4df2b
FlorianElangue <florian.elangue@hotmail.com>
2026-02-15 16:21:21 +0000

    daily_weather model created

---
+++ macros/date_utils.sql
@@ -0,0 +1,35 @@
+--Current date: Present OR Past
+{% macro present_or_past(x) %}
+
+CASE WHEN TO_TIMESTAMP({{x}}) < CURRENT_DATE THEN 'PAST'
+ELSE 'FUTURE' END
+
+{% endmacro %}
+
+--Season
+{% macro get_season(x) %}
+
+CASE
+    WHEN MONTH(TO_TIMESTAMP({{x}})) in (12, 1, 2)
+    THEN 'WINTER'
+    WHEN MONTH(TO_TIMESTAMP({{x}})) in (3, 4, 5)
+    THEN 'SPRING'
+    WHEN MONTH(TO_TIMESTAMP({{x}})) in (6, 7, 8)
+    THEN 'SUMMER'
+    ELSE 'AUTUMN'
+    END
+
+{% endmacro %}
+
+--Day type name
+{% macro day_type(x) %}
+
+CASE
+WHEN DAYNAME(TO_TIMESTAMP({{x}})) in ('Sat', 'Sun')
+THEN 'WEEKEND'
+ELSE 'BUSINESSDAY'
+END
+
+{% endmacro %}
+
+
